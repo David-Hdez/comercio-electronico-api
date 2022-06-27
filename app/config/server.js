db4dcc321d483b876a6f1850da6d0558108575ad
David Pacheco <54561496+David-Hdez@users.noreply.github.com>
2022-06-26 19:38:13 -0500

    configuration dump

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'eb9f8a4ff270106360367f2a5ca71779'),
-    },
-  },
-});
--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'e0e163c1-fa98-4ab4-88a6-1556296b02e4'
-};
