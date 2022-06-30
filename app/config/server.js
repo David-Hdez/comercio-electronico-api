3e61bee49fd491eda2eed7500cedacdc0f4e66d5
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-06-29 21:14:39 -0500

    Creando la coleccion de categorias

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '583100efd873d00fe39a0a4bc9734826'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f4c4b002-2e82-4d03-b19c-7134d0471bb6'
+};
